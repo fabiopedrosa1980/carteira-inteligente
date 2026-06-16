e772c3ea8466fec25ba23414ac71b58d00a9b985
fabiopedrosa1980 <bito.pedrosa@gmail.com>
2026-06-16 04:28:37 -0300

    feat: registra locale pt-BR para formatar moeda no padrao brasileiro

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,18 +1,31 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideAppInitializer, inject } from '@angular/core';
+import {
+  ApplicationConfig,
+  provideBrowserGlobalErrorListeners,
+  provideAppInitializer,
+  inject,
+  LOCALE_ID,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { registerLocaleData } from '@angular/common';
+import localePt from '@angular/common/locales/pt';
 
 import { routes } from './app.routes';
 import { authInterceptor } from './interceptors/auth.interceptor';
 import { AuthService } from './services/auth.service';
+
+// Registra o locale pt-BR para que os pipes `currency` e `number` formatem
+// valores no padrão brasileiro (ex.: R$ 1.000.000,00).
+registerLocaleData(localePt);
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
     provideHttpClient(withInterceptors([authInterceptor])),
+    { provide: LOCALE_ID, useValue: 'pt-BR' },
     provideAppInitializer(() => {
       inject(AuthService).loadStoredToken();
     }),
-  ]
+  ],
 };
